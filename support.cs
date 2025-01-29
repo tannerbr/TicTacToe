71ec695c8633243d630ea62125ba1dc9c69a5a2b
Your Name <your_email@example.com>
2025-01-29 09:21:31 -0700

    Method made to determine if there is a winner.

--- support.cs
+++ support.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace TicTacToe
 {
     internal class support
@@ -24,5 +23,73 @@ namespace TicTacToe
                 if (row < 2) Console.WriteLine("---+---+---"); // row separator
             }
         }
+        
+        public (string, int) GameWinner(char[,] board)
+        {
+            string response = "";
+            int endGame = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2] && board[row, 0] != ' ')
+                {
+                    response = $"Player {board[row, 0]} has won!"; // Winner is either 'X' or 'O'
+                    endGame++;
+                }
+            } 
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] == board[1, col] && board[1, col] == board[2, col] && board[0, col] != ' ')
+                {
+                    response = $"Player {board[0, col]} has won!"; // Winner is either 'X' or 'O'
+                    endGame++;
+                }
+            }
+            // Check diagonals
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ')
+            {
+                response = $"Player {board[0, 0]} has won!"; // Winner is either 'X' or 'O'
+                endGame++;
+            }
+
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ')
+            {
+                response = $"Player {board[0, 2]} has won!"; // Winner is either 'X' or 'O'
+                endGame++;
+            }
+            // Check for a tie (if there is no winner and the board is full)
+            if (endGame == 0)
+            {
+                bool boardFull = true;
+
+                // Loop through the board to check if any spaces are empty
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (board[row, col] == ' ') // Found an empty space
+                        {
+                            boardFull = false;
+                            break; // No need to check further
+                        }
+                    }
+                    if (!boardFull) break; // If found empty space, exit the row loop
+                }
+
+                if (boardFull)
+                {
+                    response = "It's a tie!";
+                    endGame++;
+                }
+            }
+            // No winner yet
+            return (response, endGame); 
+        }
+        
+        }
     }
-}
+
+
+
+
+
