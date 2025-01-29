9908d3b44dd77c348a65e9d42cd7dd58b4de29ec
carhorne <carsonhorne@gmail.com>
2025-01-29 08:46:11 -0700

    created a class that prints the game board

--- support.cs
+++ support.cs
@@ -9,11 +9,20 @@ namespace TicTacToe
     internal class support
     {
 
-        public static string printBoard(string[] board)
+        public static void printBoard(char[,] moves)
         {
-            
-            
-            return string.Join(" ", board); 
+            Console.WriteLine("Tic-Tac-Toe Board: ");
+
+            for (int row = 0; row < 3; row++) // iterate through each row in the array
+            {
+                for (int col = 0; col < 3; col++) // iterate through each column in the row
+                {
+                    Console.Write($" {moves[row, col]} "); // write the move in that position of the array
+                    if (col < 2) Console.Write("|"); // column separator
+                }
+                Console.WriteLine(); // finish the line for the row once all columns are printed out
+                if (row < 2) Console.WriteLine("---+---+---"); // row separator
+            }
         }
     }
 }
