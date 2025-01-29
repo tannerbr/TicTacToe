d6b1f5259567272542fa73acd142d1a5cb0ac673
carhorne <carsonhorne@gmail.com>
2025-01-29 08:33:52 -0700

    made a support class

---
+++ support.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class support
+    {
+
+        public static string printBoard(string[] board)
+        {
+            
+            
+            return string.Join(" ", board); 
+        }
+    }
+}
