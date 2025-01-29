8871a33d0ef2fee6c993483c6bff10a40ef5cbed
carson46 <carson46@byu.edu>
2025-01-29 08:28:15 -0700

    set up notes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// Welcome User
 
+//• Create a game board array to store the players’ choices
+
+// Ask each player in turn for their choice and update the game board array
+
+//• Print the board by calling the method in the supporting class
+
+//Check for a winner by calling the method in the supporting class, and notify the players
+// when a win has occurred and which player won the game
